0cd5e209d9717e9b537541c1a862853654f79b10
saurabh <saurabhmehta1601@gmail.com>
2022-12-12 11:00:47 +0530

    feat : Add ambientLight

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <Canvas shadows style={{ width: window.innerWidth, height: window.innerHeight }}>
       <Suspense fallback={<Loading />}>
+        <ambientLight intensity={0.1} />
         <Scene />
       </Suspense>
     </Canvas>
